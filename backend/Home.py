37eb1c114f4b23170f024197c96861f77d6a1046
srivathsankr7 <srivathsankr7@gmail.com>
2017-06-18 20:56:57 +0530

    Add insert and read expenses logic Add database queries

--- backend/Home.py
+++ backend/Home.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, request, json, jsonify
+import expenses
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Welcome to Bugget Tracker"
+    return 'Welcome to Budget Tracker'
 
+
+@app.route("/allexpences")
+def all_expences():
+    jsonresult = ''
+    try:
+        expenseList = []
+        expenceRecords = expenses.get_all_expenses()
+        for record in expenceRecords:
+            expense = "{'username': %s,'category': %s,'description': %s,'price': %d,'timestamp': %d}" %(record.username,record.category,record.description,record.price,record.record_ts)
+            expenseList.append(expense)
+            print('Record ' + expense)
+        jsonresult = json.dumps(expenseList)
+    except Exception as e:
+        print('Error in parsing json ', e)
+    return jsonify(users=jsonresult)
+
+
+@app.route("/addexpense", methods=['post'])
+def add_expense():
+    expenserecord = request.get_json()
+    #expense = json.loads(expenserecord)
+    expenses.save_expense(expenserecord)
+    return 'Ok'
+
+
+if __name__ == '__main__':
+    app.run()
